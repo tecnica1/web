72638b9d9aaecbbad2ac9990b3ac672d2d4881d0
Juan Martin <juanmaac321123@gmail.com>
2016-08-31 20:24:21 -0300

    Imagenes y jQuery

--- app/router.php
+++ app/router.php
@@ -4,4 +4,8 @@ $app->get('/', function ($request, $response) use($app) {
     return $this->view->render($response, 'pagina/index.html');
 });
 
+$app->get('/basico', function ($request, $response) use($app) {
+    return $this->view->render($response, 'pagina/orientaciones/basico.html');
+});
+
 ?>
