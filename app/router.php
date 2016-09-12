adab0e3613b1a8cef04445a6c01df249a7ab17cf
cramer28 <cramer9858@outlook.com>
2016-09-12 12:44:42 -0300

    Agregue links
    agregue los links de construccion, turismo e informatica al router,
    todavia faltan las paginas pero en eso estamos

--- app/router.php
+++ app/router.php
@@ -8,4 +8,16 @@ $app->get('/basico', function ($request, $response) use($app) {
     return $this->view->render($response, 'pagina/orientaciones/basico.html');
 });
 
+$app->get('/informatica', function ($request, $response) use($app) {
+    return $this->view->render($response, 'pagina/orientaciones/informatica.html');
+});
+
+$app->get('/construccion', function ($request, $response) use($app) {
+    return $this->view->render($response, 'pagina/orientaciones/construccion.html');
+});
+
+$app->get('/turismo', function ($request, $response) use($app) {
+    return $this->view->render($response, 'pagina/orientaciones/turismo.html');
+});
+
 ?>
